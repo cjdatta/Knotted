a7ea61af659592fd3ff60fadfe70f04914eadae7
cjdatta <aswccs@gmail.com>
2015-07-06 08:34:04 +0100

    Overhauled the display system. Now uses Views instead of canvas, easier for future work. Added bitmap tools which includes scaling, and combining of 2 bitmaps, etc.
    Can now create multiple copies of bitmap when touched, and move them around.
    
    Updated Layout XML
    
    Added placeholder resources

---
+++ app/src/main/java/chris/knotted/BitmapTools.java
@@ -0,0 +1,126 @@
+package chris.knotted;
+
+import android.content.res.Resources;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.Paint;
+
+/**
+ * Created by Chris on 06/07/2015.
+ */
+public abstract class BitmapTools {
+
+    public static Bitmap drawBeziers(int[] left, int[] right, int reqH){
+
+        Paint paint = new Paint();
+        paint.setStyle(Paint.Style.FILL_AND_STROKE);
+        paint.setAntiAlias(true);
+        paint.setStrokeWidth(2);
+        paint.setColor(Color.BLACK);
+
+        int w = 100, h = reqH;
+
+        Bitmap.Config conf = Bitmap.Config.ARGB_8888; // see other conf types
+        Bitmap bmp = Bitmap.createBitmap(w, h, conf); // MUTABLE bitmap
+        Canvas canvas = new Canvas(bmp);
+        canvas.drawColor(Color.WHITE);
+        for (int i = 0; i<left.length; i++){
+            canvas.drawLine(0, left[i], 100, right[i], paint);
+        }
+    return Bitmap.createBitmap(bmp);
+    }
+
+    /* The following two methods were reused from
+    http://developer.android.com/training/displaying-bitmaps/load-bitmap.html
+    and when used will scale bitmaps appropriately
+     */
+
+    public static int calculateInSampleSize(
+            BitmapFactory.Options options, int reqWidth, int reqHeight) {
+        // Raw height and width of image
+        final int height = options.outHeight;
+        final int width = options.outWidth;
+        int inSampleSize = 1;
+
+        if (height > reqHeight || width > reqWidth) {
+
+            final int halfHeight = height / 2;
+            final int halfWidth = width / 2;
+
+            // Calculate the largest inSampleSize value that is a power of 2 and keeps both
+            // height and width larger than the requested height and width.
+            while ((halfHeight / inSampleSize) > reqHeight
+                    && (halfWidth / inSampleSize) > reqWidth) {
+                inSampleSize *= 2;
+            }
+        }
+
+        return inSampleSize;
+    }
+
+    public static Bitmap decodeSampledBitmapFromResource(Resources res, int resId,
+                                                         int reqWidth, int reqHeight) {
+
+        // First decode with inJustDecodeBounds=true to check dimensions
+        final BitmapFactory.Options options = new BitmapFactory.Options();
+        options.inJustDecodeBounds = true;
+        BitmapFactory.decodeResource(res, resId, options);
+
+        // Calculate inSampleSize
+        options.inSampleSize = calculateInSampleSize(options, reqWidth, reqHeight);
+
+        // Decode bitmap with inSampleSize set
+        options.inJustDecodeBounds = false;
+        return BitmapFactory.decodeResource(res, resId, options);
+    }
+
+    public static Bitmap combineImagesHorizontal(Bitmap c, Bitmap s) {
+        Bitmap cs = null;
+
+        int width, height = 0;
+
+        if(c.getWidth() > s.getWidth()) {
+            width = c.getWidth() + s.getWidth();
+            height = c.getHeight();
+        } else {
+            width = s.getWidth() + s.getWidth();
+            height = c.getHeight();
+        }
+
+        cs = Bitmap.createBitmap(width, height, Bitmap.Config.ARGB_8888);
+
+        Canvas comboImage = new Canvas(cs);
+
+        comboImage.drawBitmap(c, 0f, 0f, null);
+        comboImage.drawBitmap(s, c.getWidth(), 0f, null);
+
+        return cs;
+    }
+
+    public Bitmap combineImagesVertical(Bitmap c, Bitmap s) {
+        Bitmap cs = null;
+
+        int width, height = 0;
+
+        if(c.getWidth() > s.getWidth()) {
+            width = c.getWidth();
+            height = c.getHeight() + s.getHeight();
+        } else {
+            width = s.getWidth();
+            height = c.getHeight() + s.getHeight();
+        }
+
+        cs = Bitmap.createBitmap(width, height, Bitmap.Config.ARGB_8888);
+
+        Canvas comboImage = new Canvas(cs);
+
+        comboImage.drawBitmap(c, 0f, 0f, null);
+        comboImage.drawBitmap(s, 0f, c.getHeight(), null);
+
+        return cs;
+    }
+
+
+}
--- app/src/main/java/chris/knotted/OpenGLES20Activity.java
+++ app/src/main/java/chris/knotted/OpenGLES20Activity.java
@@ -2,16 +2,22 @@ package chris.knotted;
 
 import android.app.Activity;
 import android.os.Bundle;
-import android.util.Log;
-import android.view.Window;
-import android.view.WindowManager;
+
+import android.view.MotionEvent;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.RelativeLayout;
+
+import com.example.android.opengl.R;
+
 
 /**
  * Created by Chris on 29/06/2015.
  */
-public class OpenGLES20Activity extends Activity {
+public class OpenGLES20Activity extends Activity implements View.OnTouchListener {
 
-    private MainPanel myPanel;
+/*    private MainPanel myPanel;
     private static final String TAG = OpenGLES20Activity.class.getSimpleName();
 
     @Override
@@ -23,7 +29,110 @@ public class OpenGLES20Activity extends Activity {
         myPanel = new MainPanel(this);
         setContentView(myPanel);
     }
+*/
+
+    private ImageView mCrossView, mIdenView, mSwapView;
+    private ImageView myImgView;
+    private OpenGLES20Activity parent = this;
+    private ViewGroup mRrootLayout;
+    private int xDelta;
+    private int yDelta;
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+        mRrootLayout = (ViewGroup) findViewById(R.id.root);
+
+        //init the ImageViews
+        mCrossView = (ImageView) mRrootLayout.findViewById(R.id.crossView);
+        mSwapView = (ImageView) mRrootLayout.findViewById(R.id.swapView);
+        mIdenView = (ImageView) mRrootLayout.findViewById(R.id.identityView);
+        //set their images
+        mSwapView.setImageBitmap(BitmapTools.decodeSampledBitmapFromResource(getResources(),
+                R.drawable.swap, 40, 40));
+        mCrossView.setImageBitmap(BitmapTools.decodeSampledBitmapFromResource(getResources(),
+                R.drawable.cross, 40, 40));
+        mIdenView.setImageBitmap(BitmapTools.decodeSampledBitmapFromResource(getResources(),
+                R.drawable.identity, 40, 40));
+        //set touch listeners
+        //crossview is the lab rat for the drawing method
+        mCrossView.setOnTouchListener(new View.OnTouchListener() {
+            @Override
+            public boolean onTouch(View v, MotionEvent event) {
+                if (event.getAction() == MotionEvent.ACTION_DOWN) {
+                    myImgView = new ImageView(parent);
+                    myImgView.setLayoutParams(new RelativeLayout.LayoutParams(ViewGroup.LayoutParams.WRAP_CONTENT,
+                            ViewGroup.LayoutParams.WRAP_CONTENT));
+                    myImgView.setOnTouchListener(parent);
+                    int[] a = new int[2];
+                    int[] b = new int[2];
+                    a[0] = 10;
+                    a[1] = 20;
+                    b[0] = 50;
+                    b[1] = 30;
+
+                    myImgView.setImageBitmap(
+                            BitmapTools.drawBeziers(a, b, BitmapTools.decodeSampledBitmapFromResource(getResources(),
+                                    R.drawable.swap, 30, 30).getHeight()));
+                    RelativeLayout rl = (RelativeLayout) (findViewById(R.id.root));
+                    rl.addView(myImgView);
+                    myImgView.setTop(500);
+                    myImgView.setLeft(500);
+                }
+                return true;
+            }
+        });
+        //mIdenView.setLayoutParams(layoutParamsB);
+        mIdenView.setOnTouchListener(new View.OnTouchListener() {
+            @Override
+            public boolean onTouch(View v, MotionEvent event) {
+                if (event.getAction() == MotionEvent.ACTION_DOWN){
+                    myImgView = new ImageView(parent);
+                    myImgView.setLayoutParams(new RelativeLayout.LayoutParams(150, 150));
+                    myImgView.setOnTouchListener(parent);
+                    myImgView.setImageBitmap(BitmapTools.decodeSampledBitmapFromResource(getResources(),
+                            R.drawable.identity, 30, 30));
+                    ((RelativeLayout)(findViewById(R.id.root))).addView(myImgView);
+                    myImgView.setTop(500);
+                    myImgView.setLeft(500);
+                }
+                return true;
+            }
+        });
 
 
+    }
+
+    public boolean onTouch(View view, MotionEvent event) {
+        final int X = (int) event.getRawX();
+        final int Y = (int) event.getRawY();
+        switch (event.getAction() & MotionEvent.ACTION_MASK) {
+            case MotionEvent.ACTION_DOWN:
+                RelativeLayout.LayoutParams lParams =
+                        (RelativeLayout.LayoutParams) view.getLayoutParams();
+                xDelta = X - lParams.leftMargin;
+                yDelta = Y - lParams.topMargin;
+                break;
+            case MotionEvent.ACTION_UP:
+
+                break;
+            case MotionEvent.ACTION_POINTER_DOWN:
+                break;
+            case MotionEvent.ACTION_POINTER_UP:
+                break;
+            case MotionEvent.ACTION_MOVE:
+                RelativeLayout.LayoutParams layoutParams =
+                        (RelativeLayout.LayoutParams) view.getLayoutParams();
+                layoutParams.leftMargin = X - xDelta;
+                layoutParams.topMargin = Y - yDelta;
+                layoutParams.rightMargin = -250;
+                layoutParams.bottomMargin = -250;
+                view.setLayoutParams(layoutParams);
+                break;
+        }
+        mRrootLayout.invalidate();
+        return true;
+    }
 
 }
